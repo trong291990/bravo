95f77c018eb87bd692b0585c0cbf1ed66378915a
vienlemai <vienlm@ilucians.com>
2014-08-19 18:03:47 +0700

    fix font end

--- app/controllers/TourController.php
+++ app/controllers/TourController.php
@@ -1,9 +1,8 @@
 <?php
 
 class TourController extends FrontendBaseController {
-    public function area($slug,$id) {
-		print_r($id);die();
-        $area = Area::find($id);
+    public function area($slug) {
+        $area = Area::where('slug','=',trim($slug))->first();
         $tours = $area->tours;
         $this->layout->content = View::make('frontend.tours.area')->with('tours', $tours);
     }
--- app/routes/frontend_routes.php
+++ app/routes/frontend_routes.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::get('/', array('as' => 'root', 'uses' => 'HomeController@landing'));
-Route::get('/tours/view/{slug}{id}', array('as' => 'root', 'uses' => 'TourController@area'));
+Route::get('/tours/{slug}', array('as' => 'root', 'uses' => 'TourController@area'));
 
 
