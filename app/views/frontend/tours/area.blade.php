390fca3aa352be014b0c60819cf809a9d58f2462
= <=>
2014-08-26 00:30:46 +0700

    map:

--- app/views/frontend/tours/area.blade.php
+++ app/views/frontend/tours/area.blade.php
@@ -60,7 +60,9 @@
                 </div>
             </div>
             <div id="tours-list">
-                <?php foreach($tours as $tour): ?>
+                <?php foreach($tours as $tour): 
+                    
+                ?>
                 <div class="tour-item" >
                     <div class="tour-sliders">
                         @if($tour->photo)
@@ -90,8 +92,8 @@
                         </div>
                         <div class="col-sm-4 clearfix">
                             <div class="col-sm-6 no-padding-left">
-                                <canvas class="tour-map" style="width: 156px;height: 156px" data-places='<?php echo json_encode($tour->places->lists('name')) ?>'>
-                                </canvas>
+                                <div data-id="{{$tour->id}}" id="tour-map-{{$tour->id}}" class="tour-map" style="width: 156px;height: 156px">
+                                </div>
                             </div>
                             <div class="col-sm-6 no-padding-right tour-actions">
                                 <button class="btn btn-block btn-warning tour-booking">Booking now</button>
@@ -107,78 +109,71 @@
             <div class="clearfix">
                 <?php echo $tours->links(); ?>
             </div>
+           <div class="modal fade" id="map-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                      <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div id='map-modal-content' style="width: 100%;height: 400px"></div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+            </div>
+@stop
+@section('addon_js')
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
 @stop
 @section('inline_scripts')
 <script>
-    jQuery(function($) {
-        // Asynchronously Load the map API 
-        var script = document.createElement('script');
-        script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
-        document.body.appendChild(script);
-        function showMap(dom) {
-            var map;
-            var bounds = new google.maps.LatLngBounds();
-            var mapOptions = {
-                mapTypeId: 'roadmap'
-            };
+    function showMap(dom,locations,zoom){
+            var map = new google.maps.Map(dom, {
+             zoom:zoom,
+             center: new google.maps.LatLng(locations[0][1],locations[0][2]),
+             mapTypeId: google.maps.MapTypeId.ROADMAP
+           });
 
-            // Display a map on the page
-            map = new google.maps.Map(dom, mapOptions);
-            map.setTilt(45);
+           var infowindow = new google.maps.InfoWindow();
+           var marker, i;
 
-            // Multiple Markers
-            var markers = [
-                ['London Eye, London', 51.503454,-0.119562],
-                ['Palace of Westminster, London', 51.499633,-0.124755]
-            ];
-
-            // Info Window Content
-            var infoWindowContent = [
-                ['<div class="info_content">' +
-                '<h3>London Eye</h3>' +
-                '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' +        '</div>'],
-                ['<div class="info_content">' +
-                '<h3>Palace of Westminster</h3>' +
-                '<p>The Palace of Westminster is the meeting place of the House of Commons and the House of Lords, the two houses of the Parliament of the United Kingdom. Commonly known as the Houses of Parliament after its tenants.</p>' +
-                '</div>']
-            ];
-
-            // Display multiple markers on a map
-            var infoWindow = new google.maps.InfoWindow(), marker, i;
-
-            // Loop through our array of markers & place each one on the map  
-            for( i = 0; i < markers.length; i++ ) {
-                var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-                bounds.extend(position);
+            for (i = 0; i < locations.length; i++) {  
                 marker = new google.maps.Marker({
-                    position: position,
-                    map: map,
-                    title: markers[i][0]
+                  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                  map: map,
+                  icon: "{{ URL::asset('/frontend/images/common/map.png')}}",
                 });
 
-                // Allow each marker to have an info window    
                 google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    return function() {
-                        infoWindow.setContent(infoWindowContent[i][0]);
-                        infoWindow.open(map, marker);
-                    }
+                  return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                  }
                 })(marker, i));
-
-                // Automatically center the map fitting all markers on the screen
-                map.fitBounds(bounds);
-            }
-
-            // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-            var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-                this.setZoom(14);
-                google.maps.event.removeListener(boundsListener);
-            });
+           }
         }
-        
-        $('.tour-map').each(function(){
-            showMap(this);
-        });
+    <?php foreach($tours as $tour):
+        $places = [];
+        foreach($tour->places as $place){
+            $places[] = [$place->name,$place->lat,$place->lng];
+        }
+    ?>
+        var tour{{$tour->id}}_places = {{json_encode($places)}};
+        showMap(document.getElementById('tour-map-{{$tour->id}}'),tour{{$tour->id}}_places,3); 
+    <?php endforeach;?>
+    $('.tour-map').on('click',function(){
+         var id = $(this).data('id');
+         var localtionModal = eval("tour"+id+"_places");
+         var title = $(this).parents('.tour-content').find('h3').text();
+         $('#map-modal .modal-title').text(title);
+         $('#map-modal').modal('show');
+         $('#map-modal').on('shown.bs.modal', function (e) {
+             showMap(document.getElementById('map-modal-content'),localtionModal,7);
+         });
     });
-    
 </script>
 @stop
