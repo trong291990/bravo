cd059dc8743376f276b0b25561153a223ada79f4
fernolder <fern.older@gmail.com>
2014-09-12 15:04:15 +0700

    try fix

--- app/models/Setting.php
+++ app/models/Setting.php
@@ -2,17 +2,17 @@
 
 class Setting {
 
-    const TERMS_CONTENT_FILE_PATH = 'app/views/frontend/pages/html/terms_and_condition.php';
+    const TERMS_CONTENT_FILE_PATH = '/views/frontend/pages/html/terms_and_condition.php';
 
     public static $EMAIL_RECEIVE_NOTIFY = 'support@bravoindochinatour.com';
 
     public static function updateTermsContent($content) {
-        create_file_if_not_exists(self::TERMS_CONTENT_FILE_PATH);
+        create_file_if_not_exists(app_path() . self::TERMS_CONTENT_FILE_PATH);
         file_put_contents(self::TERMS_CONTENT_FILE_PATH, $content);
     }
 
     public static function getTermsContent() {
-        create_file_if_not_exists(self::TERMS_CONTENT_FILE_PATH);
+        create_file_if_not_exists(app_path() . self::TERMS_CONTENT_FILE_PATH);
         return file_get_contents(self::TERMS_CONTENT_FILE_PATH);
     }
 
