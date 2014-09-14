de82e9dbe0783717d72d0bf0a9e66ef4a58ccbc3
cainoigithe <cainoigithe@fk.com>
2014-09-15 00:46:12 +0700

    update menu

--- app/views/layouts/frontend.blade.php
+++ app/views/layouts/frontend.blade.php
@@ -20,18 +20,18 @@
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
-         @yield('addon_stylesheets')
+        @yield('addon_stylesheets')
     </head>
     <body>
         <div id="header" @yield('area_class')>
-            <div role="navigation" class="navbar navbar-default" id="main-nav-warrper" >
+             <div role="navigation" class="navbar navbar-default" id="main-nav-warrper" >
                 <div class="container">
                     <div class="navbar-header">
                         <button data-target="#main-nav" data-toggle="collapse" class="navbar-toggle" type="button">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
                         </button>
                         <a href="#" class="navbar-brand" id="main-logo">
                             <img class="img-responsive" src="{{ URL::asset('/') }}frontend/images/page/main_logov2.jpg">
@@ -40,28 +40,38 @@
                     </div>
                     <div class="navbar-collapse collapse" id="main-nav">
                         <ul class="nav navbar-nav pull-right">
-                          <li class="active"><a href="{{Request::root()}}">Home</a></li>
-                          <li><a href="{{Request::root()}}/about-us">About</a></li>
-                          <li><a href="{{Request::root()}}/contact">Contact</a></li>
-                          <li><a href="{{route('terms')}}">Term & Condition</a></li>
-                          <li><a href="{{route('review')}}">Reviews</a></li>
-                          <li>
-                              <form id="search-form" action="{{route('tour.search')}}">
-                                  <input type="text" placeholder="Search" name="keyword" value="{{Input::get('keyword')}}"/>
-                                  <button type="submit"><i class="fa fa-search"></i></button>
-                              </form>
-                          </li>
+                            <li class="active"><a href="{{Request::root()}}">Home</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Why travel with us</a></li>
+                                    <li><a href="{{route('terms')}}">Term & Condition</a></li>
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">Join Our Team</a></li>
+                                    <li><a href="#">Travel Album</a></li>
+                                    <li><a href="{{route('about_us')}}">About Us</a></li>
+                                </ul>
+                            </li>      
+                            <li><a href="{{Request::root()}}/indochina-travel-blog">Blog</a></li>
+                            <li><a href="{{route('review')}}">Reviews</a></li>
+                            <li><a href="{{Request::root()}}/contact">Contact</a></li>
+                            <li>
+                                <form id="search-form" action="{{route('tour.search')}}">
+                                    <input type="text" placeholder="Search" name="keyword" value="{{Input::get('keyword')}}"/>
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </li>
                         </ul>
                     </div><!--/.nav-collapse -->
                     <div id="country-main-nav-wrapper">
-                         <ul class="list-unstyled list-inline">
+                        <ul class="list-unstyled list-inline">
                             <li><a href="{{Request::root()}}/tours/indochina-tours">Indochina</a></li>
                             <li><a href="{{Request::root()}}/tours/vietnam-tours">Vietnam</a></li>
                             <li><a href="{{Request::root()}}/tours/cambodia-tours">Cambodia</a></li>
                             <li><a href="{{Request::root()}}/tours/laos-tours">Lao</a></li>
-                         </ul>
+                        </ul>
                     </div>
-            </div>
+                </div>
             </div>
             <div class="container" id="main-captions">
                 <div class="col-lg-3 col-lg-offset-9 col-sm-6 col-sm-offset-6 no-padding">
@@ -69,7 +79,7 @@
                         <p>Customize Your Indochina Trip </p>
                         <p class="small">Easy steps to a free <span class="underline">custom itinerary</span></p>
                         <div class="clearfix">
-                        <a class="btn btn-default btn-flat pull-right" href="{{route('inquiry.create')}}">Start now</a>
+                            <a class="btn btn-default btn-flat pull-right" href="{{route('inquiry.create')}}">Start now</a>
                         </div>
                     </div>
                 </div>
@@ -92,15 +102,15 @@
                         <p>Checkout what other travelers <br/>Bravo us On TripAdvisor</p>
                         <div>
                             <a target="_blank" href="http://www.tripadvisor.com/Attraction_Review-g293924-d7057241-Reviews-Bravo_Tours_Day_Tours-Hanoi.html">
-                                 <img class="img-responsive" src="{{ URL::asset('/') }}frontend/images/page/tripadvisor.png">
+                                <img class="img-responsive" src="{{ URL::asset('/') }}frontend/images/page/tripadvisor.png">
                             </a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-2">
-                         <p>Checkout what our client said <br/> &nbsp;</p>
+                        <p>Checkout what our client said <br/> &nbsp;</p>
                         <div>
-                             <a href="http://www.reviewcentre.com/Tour-Companies/Bravo-Indochina-Tours-www-bravoindochinatour-com-reviews_2826805">
-                                 <img class="img-responsive" src="{{ URL::asset('/') }}frontend/images/page/review_cente.png">
+                            <a href="http://www.reviewcentre.com/Tour-Companies/Bravo-Indochina-Tours-www-bravoindochinatour-com-reviews_2826805">
+                                <img class="img-responsive" src="{{ URL::asset('/') }}frontend/images/page/review_cente.png">
                             </a>
                         </div>
                     </div>
@@ -129,72 +139,72 @@
         </div>
         <div class="modal fade" id="booking-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                  <h2 class="modal-title" style="text-align: center" id="myModalLabel">Send us an Enquiry</h2>
-                </div>
-               <?php echo Former::open('/booking') ?>
-                <div class="modal-body">
-                    <div id='booking-modal-content' class='row clearfix'>
-                        <div  class='col-sm-10 col-sm-offset-1'>
-                            <input type="hidden" value="" id='booking-tour-id' name='tour_id' />
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Your name(*)</label>
-                                <input type="text" class="form-control" name='customer_name' required="required"  placeholder="Enter your name">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Your email(*)</label>
-                                <input type="email" name='customer_email' class="form-control" required="required"  placeholder="Enter your email">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Travel date</label>
-                                <input name='start_date' type="text" 
-                                       class="form-control datepicker" 
-                                        placeholder="Enter your travel date">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">How many traveling ?</label>
-                                <input name='travelling' type="text" 
-                                       class="form-control"
-                                       placeholder="Enter your number of  traveling">
-                            </div>
-                             <div class="form-group">
-                                <label for="exampleInputEmail1">Message</label>
-                                <textarea name='message' class="form-control" placeholder="Your message" style="min-height: 120px"></textarea>
-                            </div>
-                            </div>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                        <h2 class="modal-title" style="text-align: center" id="myModalLabel">Send us an Enquiry</h2>
                     </div>
+                    <?php echo Former::open('/booking') ?>
+                    <div class="modal-body">
+                        <div id='booking-modal-content' class='row clearfix'>
+                            <div  class='col-sm-10 col-sm-offset-1'>
+                                <input type="hidden" value="" id='booking-tour-id' name='tour_id' />
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Your name(*)</label>
+                                    <input type="text" class="form-control" name='customer_name' required="required"  placeholder="Enter your name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Your email(*)</label>
+                                    <input type="email" name='customer_email' class="form-control" required="required"  placeholder="Enter your email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Travel date</label>
+                                    <input name='start_date' type="text" 
+                                           class="form-control datepicker" 
+                                           placeholder="Enter your travel date">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">How many traveling ?</label>
+                                    <input name='travelling' type="text" 
+                                           class="form-control"
+                                           placeholder="Enter your number of  traveling">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Message</label>
+                                    <textarea name='message' class="form-control" placeholder="Your message" style="min-height: 120px"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Send now</button>   
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                    <?php echo Former::close(); ?>
                 </div>
-                <div class="modal-footer">
-                  <button type="submit" class="btn btn-primary">Send now</button>   
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-               <?php echo Former::close(); ?>
-              </div>
             </div>
         </div>
         <div class="modal fade" id="booking-success" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
+            <div class="modal-dialog">
+                <div class="modal-content">
                     <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                      <h3>Booking success</h3>
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                        <h3>Booking success</h3>
                     </div>
-                      <div class="modal-body" style="padding: 0">
-                          <div id='success-modal-content' style="margin-top:20px">
+                    <div class="modal-body" style="padding: 0">
+                        <div id='success-modal-content' style="margin-top:20px">
                             <div role="alert" class="alert alert-success">
-                            <strong>Well done!</strong> Your booking request has been sent. We will contact with you in 2 hours.
-                          </div>
-                            
+                                <strong>Well done!</strong> Your booking request has been sent. We will contact with you in 2 hours.
+                            </div>
+
                         </div>
                     </div>
-                      <div class="modal-footer" style="margin: 0">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <div class="modal-footer" style="margin: 0">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     </div>
-                  </div>
                 </div>
             </div>
+        </div>
         {{ HTML::script('/shared/js/jquery-2.0.3.min.js') }}
         {{ HTML::script('/shared/js/bootstrap.min.js') }}
         {{ HTML::script('/plugins/bootstrap-select/bootstrap-select.min.js') }}
@@ -213,7 +223,7 @@
         </script>
         @endif
         @yield('addon_js')
-        
+
         @yield('inline_scripts')
     </body>
 </html>
