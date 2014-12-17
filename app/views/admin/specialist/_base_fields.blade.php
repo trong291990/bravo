3fc198239a0e60477532d2f0807adcdb7dce3051
fernolder <fernolder@gmail.com>
2014-12-17 23:53:37 +0700

    up

--- app/views/admin/specialist/_base_fields.blade.php
+++ app/views/admin/specialist/_base_fields.blade.php
@@ -6,10 +6,30 @@
 <?php
 if (isset($specialist)) {
     $specialist_area_ids = $specialist->areas()->lists('id');
+    $specialties = $specialist->specialties;
 } else {
     $specialist_area_ids = [];
+    $specialties = [];
 }
 ?>
+<div class="form-group">
+    <label class="control-label col-lg-2 col-sm-3">Specialties</label>
+    <div class="col-lg-10 col-sm-9">
+        <ul class="list-unstyled staff-specialties">
+            @foreach($specialties as $item)
+                <li>
+                    <input class="form-control" name="specialties[]" value="{{$item}}" style="margin-bottom: 10px" />
+                </li>
+            @endforeach
+            @for($i = 0; $i < (Specialist::MAX_SPECIALTIEST - count($specialties)); $i++)
+                <li>
+                    <input class="form-control" name="specialties[]" style="margin-bottom: 10px"/>
+                </li>
+            @endfor                         
+        </ul>        
+    </div>
+</div> 
+
 <div class='form-group'> 
     <label for="email" class="control-label col-lg-2 col-sm-3">Countries</label>
     <div class="col-lg-10 col-sm-9">
